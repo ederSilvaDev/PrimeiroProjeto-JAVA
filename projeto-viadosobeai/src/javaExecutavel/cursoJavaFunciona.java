4c03f1eb53424f9638a82d397b3b6c6713577b2e
ederSilvaDev <81658662+ederSilvaDev@users.noreply.github.com>
2021-04-10 19:25:03 -0300

    GitHub Básico #1 - Criar e Excluir um REPOSITÓRIO no GitHub

---
+++ projeto-viadosobeai/src/javaExecutavel/cursoJavaFunciona.java
@@ -0,0 +1,10 @@
+package javaExecutavel;
+
+public class cursoJavaFunciona {
+
+	public static void main(String[] args) {
+		System.out.println("Projeto Java Viado que não Sobe no Git");
+			
+	}
+
+}
